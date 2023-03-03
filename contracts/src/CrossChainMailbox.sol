36c608010246b1c7dd8b98c739d4818bc2270cef
mattstam <mattstam@live.com>
2023-03-02 16:22:43 -0800

    ENS with local node and namehash that should work

--- contracts/src/CrossChainMailbox.sol
+++ contracts/src/CrossChainMailbox.sol
@@ -8,6 +8,7 @@ import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 import {ENS} from "ens-contracts/registry/ENS.sol";
 import {Resolver} from "ens-contracts/resolvers/Resolver.sol";
 import {IReverseRegistrar} from "ens-contracts/registry/IReverseRegistrar.sol";
+import {BytesUtils} from "ens-contracts/wrapper/BytesUtils.sol";
 
 struct Message {
     uint32 sourceChain;
@@ -15,39 +16,9 @@ struct Message {
     string message;
 }
 
-abstract contract Resolver {
-    function addr(bytes32 node) public view virtual returns (address);
-}
-
-// interface IReverseRegistrar {
-//     function setDefaultResolver(address resolver) external;
-
-//     function claim(address owner) external returns (bytes32);
-
-//     function claimForAddr(
-//         address addr,
-//         address owner,
-//         address resolver
-//     ) external returns (bytes32);
-
-//     function claimWithResolver(
-//         address owner,
-//         address resolver
-//     ) external returns (bytes32);
-
-//     function setName(string memory name) external returns (bytes32);
-
-//     function setNameForAddr(
-//         address addr,
-//         address owner,
-//         address resolver,
-//         string memory name
-//     ) external returns (bytes32);
-
-//     function node(address addr) external pure returns (bytes32);
-// }
-
 contract CrossChainMailboxSender is Ownable {
+    using BytesUtils for bytes;
+
     error InsufficientFee(uint256 actual, uint256 expected);
 
     /// @notice The fee to pay for sending a message.
@@ -90,82 +61,38 @@ contract CrossChainMailboxSender is Ownable {
     // Same address for Mainet, Ropsten, Rinkerby, Gorli and other networks;
     ENS ensRegistry = ENS(0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e);
 
-    function resolve(bytes32 node) public view returns (address) {
-        Resolver resolver = ensRegistry.resolver(node);
-        return resolver.addr(node);
-    }
-    // ENS Reverse Record contract
+    function getName(address _addr) public view returns (string memory name) {
+        bytes32 node = reverseNode(_addr);
 
-    IReverseRegistrar ensReverseResolver = address(0x3671aE578E63FdF66ad4F3E12CC0c0d71Ac7510C);
-
-    // The steps, given an input address, are:
-    // 1. Construct namehash (node) for <address>.addr.reverse
-    // 2. Lookup resolver for node in registry
-    // 3. Call name on resolver to get the primary name
-    // 4. Do forward resolution on that primary name to ensure it resolves to the same address as the input address
-
-    // function reverseResolve(address _addr) internal view returns (string memory) {
-    //     ensReverseResolver.node(_addr);
-    // }
-
-    // /// @notice Find ENS names for a given address
-    // /// @param addr to lookup
-    // /// @return ENS names
-    // function getENSName(address _addr) public view returns (string[] memory) {
-    //     ReverseRecords reverseRecords = ReverseRecords(ensReverseContractLookupAddress);
-    //     address[] memory t = new address[](1); // Define a fixed length array for getNames lookup.
-    //     t[0] = _addr;
-    //     return reverseRecords.getNames(t);
-    // }
-
-    // TODO read https://docs.ens.domains/dapp-developer-guide/resolving-names#reverse-resolution
-    // Forward resolution check is important because there is no enforcement at the contract level on reverse records.
-
-    function getName(address _addr) public view returns (string memory) {
-        bytes32 node = node(_addr);
-        Resolver resolver = Resolver(ensRegistry.resolver(node));
-        if (address(resolver) != address(0x0)) {
-            Resolver resolver = Resolver(resolver);
-            return resolver.name(node);
-        }
-        return "";
-    }
-
-    function getName(address _addr) public view returns (string memory) {
-        bytes32 node = node(_addr);
-        address resolverAddress = ensRegistry.resolver(node);
-        if (resolverAddress != address(0x0)) {
-            Resolver resolver = Resolver(resolverAddress);
-            string memory name = resolver.name(node);
+        // Use reverse resolver to get the ENS name that address this has.
+        Resolver reverseResolver = Resolver(ensRegistry.resolver(node));
+        if (address(reverseResolver) != address(0x0)) {
+            name = resolver.name(node);
             if (bytes(name).length == 0) {
                 return "";
             }
-            bytes32 namehash = Namehash.namehash(name);
-            address forwardResolverAddress = ensRegistry.resolver(namehash);
-            if (forwardResolverAddress != address(0x0)) {
-                Resolver forwardResolver = Resolver(forwardResolverAddress);
-                address forwardAddress = forwardResolver.addr(namehash);
-                if (forwardAddress == _addr) {
-                    return name;
-                }
+        }
+
+        // ENS does not enforce the accuracy of reverse records, so you you must always perform a
+        // forward resolution for the returned name and check it matches the original address.
+        bytes32 namehash = namehash(name).namehash(0);
+        Resolver forwardResolver = Resolver(ensRegistry.resolver(namehash));
+        if (address(forwardResolver) != address(0x0)) {
+            address forwardAddr = forwardResolver.addr(namehash);
+            if (forwardAddr == _addr) {
+                return name;
             }
         }
+
         return "";
     }
 
-    function node(address addr) private pure returns (bytes32) {
-        return keccak256(abi.encodePacked(ADDR_REVERSE_NODE, sha3HexAddress(addr)));
-    }
+    /// @notice This is the equivalant of namehash('addr.reverse')
+    bytes32 public constant ADDR_REVERSE_NODE = 0x91d1777781884d03a6757a803996e38de2a42967fb37eeaca72729271025a9e2;
 
-    function namehash(string memory name) internal pure returns (bytes32 hash) {
-        hash = bytes32(0);
-        Strings.slice memory nameslice = name.toSlice();
-        Strings.slice memory delim = ".".toSlice();
-        Strings.slice memory token;
-        for (nameslice.rsplit(delim, token); !token.empty(); nameslice.rsplit(delim, token)) {
-            hash = keccak256(abi.encodePacked(hash, token.keccak()));
-        }
-        return hash;
+    /// @notice Returns the node hash for a given account's reverse records.
+    function reverseNode(address _addr) public pure override returns (bytes32) {
+        return keccak256(abi.encodePacked(ADDR_REVERSE_NODE, sha3HexAddress(_addr)));
     }
 }
 
@@ -183,5 +110,139 @@ contract CrossChainMailboxReceiver is TelepathyHandler {
 
     function messagesLength() external view returns (uint256) {
         return messages.length;
+    }
+}
+
+library Strings {
+    struct slice {
+        uint256 _len;
+        uint256 _ptr;
+    }
+
+    /*
+     * @dev Returns a slice containing the entire string.
+     * @param self The string to make a slice from.
+     * @return A newly allocated slice containing the entire string.
+     */
+    function toSlice(string memory self) internal pure returns (slice memory) {
+        uint256 ptr;
+        assembly {
+            ptr := add(self, 0x20)
+        }
+        return slice(bytes(self).length, ptr);
+    }
+
+    /*
+     * @dev Returns the keccak-256 hash of the slice.
+     * @param self The slice to hash.
+     * @return The hash of the slice.
+     */
+    function keccak(slice memory self) internal pure returns (bytes32 ret) {
+        assembly {
+            ret := keccak256(mload(add(self, 32)), mload(self))
+        }
+    }
+
+    /*
+     * @dev Returns true if the slice is empty (has a length of 0).
+     * @param self The slice to operate on.
+     * @return True if the slice is empty, False otherwise.
+     */
+    function empty(slice memory self) internal pure returns (bool) {
+        return self._len == 0;
+    }
+
+    // Returns the memory address of the first byte after the last occurrence of
+    // `needle` in `self`, or the address of `self` if not found.
+    function rfindPtr(uint256 selflen, uint256 selfptr, uint256 needlelen, uint256 needleptr)
+        private
+        pure
+        returns (uint256)
+    {
+        uint256 ptr;
+
+        if (needlelen <= selflen) {
+            if (needlelen <= 32) {
+                bytes32 mask = bytes32(~(2 ** (8 * (32 - needlelen)) - 1));
+
+                bytes32 needledata;
+                assembly {
+                    needledata := and(mload(needleptr), mask)
+                }
+
+                ptr = selfptr + selflen - needlelen;
+                bytes32 ptrdata;
+                assembly {
+                    ptrdata := and(mload(ptr), mask)
+                }
+
+                while (ptrdata != needledata) {
+                    if (ptr <= selfptr) {
+                        return selfptr;
+                    }
+                    ptr--;
+                    assembly {
+                        ptrdata := and(mload(ptr), mask)
+                    }
+                }
+                return ptr + needlelen;
+            } else {
+                // For long needles, use hashing
+                bytes32 hash;
+                assembly {
+                    hash := keccak256(needleptr, needlelen)
+                }
+                ptr = selfptr + (selflen - needlelen);
+                while (ptr >= selfptr) {
+                    bytes32 testHash;
+                    assembly {
+                        testHash := keccak256(ptr, needlelen)
+                    }
+                    if (hash == testHash) {
+                        return ptr + needlelen;
+                    }
+                    ptr -= 1;
+                }
+            }
+        }
+        return selfptr;
+    }
+
+    /*
+     * @dev Splits the slice, setting `self` to everything before the last
+     *      occurrence of `needle`, and `token` to everything after it. If
+     *      `needle` does not occur in `self`, `self` is set to the empty slice,
+     *      and `token` is set to the entirety of `self`.
+     * @param self The slice to split.
+     * @param needle The text to search for in `self`.
+     * @param token An output parameter to which the first token is written.
+     * @return `token`.
+     */
+    function rsplit(slice memory self, slice memory needle, slice memory token) internal pure returns (slice memory) {
+        uint256 ptr = rfindPtr(self._len, self._ptr, needle._len, needle._ptr);
+        token._ptr = ptr;
+        token._len = self._len - (ptr - self._ptr);
+        if (ptr == self._ptr) {
+            // Not found
+            self._len = 0;
+        } else {
+            self._len -= token._len + needle._len;
+        }
+        return token;
+    }
+}
+
+library Namehash {
+    using Strings for *;
+
+    function namehash(string memory name) internal pure returns (bytes32 hash) {
+        hash = bytes32(0);
+        Strings.slice memory nameslice = name.toSlice();
+        Strings.slice memory delim = ".".toSlice();
+        Strings.slice memory token;
+        for (nameslice.rsplit(delim, token); !token.empty(); nameslice.rsplit(delim, token)) {
+            hash = keccak256(abi.encodePacked(hash, token.keccak()));
+        }
+        return hash;
     }
 }
