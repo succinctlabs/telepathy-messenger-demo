526b4c75be1ea67485105ef564ff97d1a1c767ab
mattstam <mattstam@live.com>
2023-03-07 13:07:40 -0800

    deploy to mainnet and bsc too

--- contracts/deployMailboxes.sh
+++ contracts/deployMailboxes.sh
@@ -1,7 +1,10 @@
+forge script script/DeployMailbox.s.sol:DeployMailer --rpc-url $RPC_1 --broadcast --private-key $PRIVATE_KEY
 forge script script/DeployMailbox.s.sol:DeployMailer --rpc-url $RPC_5 --broadcast --private-key $PRIVATE_KEY
 forge script script/DeployMailbox.s.sol:DeployMailer --rpc-url $RPC_100 --broadcast --private-key $PRIVATE_KEY
 forge script script/DeployMailbox.s.sol:DeployMailer --rpc-url $RPC_137 --broadcast --private-key $PRIVATE_KEY
 
+forge script script/DeployMailbox.s.sol:DeployMailbox --rpc-url $RPC_1 --broadcast --private-key $PRIVATE_KEY
 forge script script/DeployMailbox.s.sol:DeployMailbox --rpc-url $RPC_5 --broadcast --private-key $PRIVATE_KEY
+forge script script/DeployMailbox.s.sol:DeployMailbox --rpc-url $RPC_56 --broadcast --private-key $PRIVATE_KEY
 forge script script/DeployMailbox.s.sol:DeployMailbox --rpc-url $RPC_100 --broadcast --private-key $PRIVATE_KEY
 forge script script/DeployMailbox.s.sol:DeployMailbox --rpc-url $RPC_137 --broadcast --private-key $PRIVATE_KEY
