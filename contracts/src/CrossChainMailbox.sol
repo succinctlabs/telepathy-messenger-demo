6ed98e250676fcfa944e6306eea8e042592f5b65
mattstam <mattstam@live.com>
2023-03-09 12:31:22 -0800

    update deps again + use source/dest

--- contracts/src/CrossChainMailbox.sol
+++ contracts/src/CrossChainMailbox.sol
@@ -19,16 +19,19 @@ contract CrossChainMailer is FeeCollector, ENSHelper {
         telepathyRouter = ITelepathyRouter(_telepathyRouter);
     }
 
-    /// @notice Sends a message to a target recipient mailbox.
-    /// @param _recipientChainId The chain ID where the target CrossChainMailbox.
-    /// @param _recipientMailbox The address of the target CrossChainMailbox.
+    /// @notice Sends a message to a destination mailbox.
+    /// @param _destinationChainId The chain ID where the destination CrossChainMailbox.
+    /// @param _destinationMailbox The address of the destination CrossChainMailbox.
     /// @param _message The message to send.
-    function sendMail(uint32 _recipientChainId, address _recipientMailbox, bytes memory _message) external payable {
+    function sendMail(uint32 _destinationChainId, address _destinationMailbox, bytes memory _message)
+        external
+        payable
+    {
         if (msg.value < fee) {
             revert InsufficientFee(msg.value, fee);
         }
         string memory data = StringHelper.formatMessage(_message, msg.sender.balance, ENSHelper.getName(msg.sender));
-        telepathyRouter.sendViaStorage(_recipientChainId, _recipientMailbox, bytes(data));
+        telepathyRouter.sendViaStorage(_destinationChainId, _destinationMailbox, bytes(data));
     }
 }
 
@@ -38,13 +41,16 @@ contract CrossChainMailer is FeeCollector, ENSHelper {
 contract CrossChainMailbox is TelepathyHandler {
     string[] public messages;
 
-    event MessageReceived(uint32 indexed sourceChain, address indexed sender, string message);
+    event MessageReceived(uint32 indexed sourceChainId, address indexed sourceAddress, string message);
 
     constructor(address _telepathyReceiever) TelepathyHandler(_telepathyReceiever) {}
 
-    function handleTelepathyImpl(uint32 _sourceChainId, address _sender, bytes memory _message) internal override {
+    function handleTelepathyImpl(uint32 _sourceChainId, address _sourceAddress, bytes memory _message)
+        internal
+        override
+    {
         messages.push(string(_message));
-        emit MessageReceived(_sourceChainId, _sender, string(_message));
+        emit MessageReceived(_sourceChainId, _sourceAddress, string(_message));
     }
 
     function messagesLength() external view returns (uint256) {
